19c81a6487e85013c8d9659300f8d298eb1b9eda
Jeff Cousins <jeffcousins@me.com>
2016-04-04 00:57:13 -0500

    Extracted store configuration. Refactored into a module function.

--- src/components/BrewContent.jsx
+++ src/components/BrewContent.jsx
@@ -1,18 +1,38 @@
 import React from 'react';
-import PostList from './PostList';
-import { connector } from '../store/store';
+
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchBrewContent } from '../actions/index';
+const { object, func } = React.PropTypes;
 
 const BrewContent = React.createClass({
   propTypes: {
-    params: React.PropTypes.object,
-    forums: React.PropTypes.object
+    params: object,
+    fetchBrewContent: func,
+    brewContent: object
   },
-  getForumData (b) {
-    return this.props.forums[b] || null;
+  componentWillMount () {
+    console.log('Inside componentWillMount in BrewContent component');
+    console.log('this.props is:');
+    console.log(this.props);
+    this.props.fetchBrewContent(this.props.params.b);
+  },
+  componentDidMount () {
+    console.log('Inside DID MOUNT in BrewContent component');
+    console.log('this.props is:');
+    console.log(this.props);
+  },
+  componentDidReceiveProps () {
+    console.log('Inside DID RECEIVE PROPS in BrewContent component');
+    console.log('this.props is:');
+    console.log(this.props);
   },
   render () {
-    const forum = this.getForumData(this.props.params.b);
-    if (forum === null) {
+    console.log('Inside render() in BrewContent component');
+    console.log('this.props is:');
+    console.log(this.props);
+
+    if (!this.props.brewContent) {
       return (
         <div>
           <h2>No results found for {this.props.params.b}</h2>
@@ -21,14 +41,25 @@ const BrewContent = React.createClass({
     } else {
       return (
         <div>
-          <h1>{forum.title}</h1>
+          <h1>Title!</h1>
           <hr />
           <h2>Posts:</h2>
-          <PostList forum={forum} />
+          <p>{this.props.brewContent.description}</p>
+          <p>TODO: render posts here!</p>
         </div>
       );
     }
   }
 });
 
-export default connector(BrewContent);
+function mapStateToProps (state) {
+  return {
+    brewContent: state.brewContent
+  };
+}
+
+function mapDispatchToProps (dispatch) {
+  return bindActionCreators({ fetchBrewContent }, dispatch);
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(BrewContent);
--- src/components/Root.jsx
+++ src/components/Root.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { Router, browserHistory } from 'react-router';
-import { store } from '../store/store';
 import createRoutes from '../routes';
+/* import { createStore, applyMiddleware } from 'redux'; */
+/* import promiseMiddleware from 'redux-promise'; */
+/* import rootReducer from '../reducers/index'; */
+import configStore from '../store/configStore';
+
+/*
+const createStoreWithMiddleware = applyMiddleware(promiseMiddleware)(createStore);
+const store = createStoreWithMiddleware(rootReducer);
+*/
+
+const store = configStore();
 
 const Root = React.createClass({
   render () {
