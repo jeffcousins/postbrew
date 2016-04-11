d6d3dd8d71b1d91cef69bb93d5eb2ebd43daf03c
Jeff Cousins <jeffcousins@me.com>
2016-04-10 22:14:15 -0500

    Removed test dropdown element

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,31 +10,11 @@ const App = React.createClass({
   propTypes: {
     children: React.PropTypes.element
   },
-  componentWillMount () {
-    if (process.env.BROWSER) {
-      $('.ui.dropdown').dropdown('refresh');
-    }
-  },
-  componentDidUpdate () {
-    $('.ui.dropdown').dropdown('refresh');
-  },
   render () {
     return (
       <div style={style}>
         <TopNav forums={this.props.forums} />
         {this.props.children || <h1>postbrew</h1>}
-
-        {/* Testing Semantic UI classes */}
-
-        <div className='ui selection dropdown'>
-          <input type='hidden' name='gender' />
-          <i className='dropdown icon'></i>
-          <div className='default text'>Dropdown</div>
-          <div className='menu'>
-            <div className='item' data-value='1'>Rocketship</div>
-            <div className='item' data-value='0'>Tortilla</div>
-          </div>
-        </div>
       </div>
     );
   }
