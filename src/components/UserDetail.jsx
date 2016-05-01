85992145fd8bbf080264be4746ce622599a8af07
Jeff Cousins <jeffcousins@me.com>
2016-05-01 18:34:57 -0400

    Create blank UserDetail component

---
+++ src/components/UserDetail.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const UserDetail = React.createClass({
+  render() {
+    return (
+      <div>
+        <h1>User Detail</h1>
+      </div>
+    );
+  }
+});
+
+export default UserDetail;
--- src/routes.jsx
+++ src/routes.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { Route } from 'react-router';
 import App from './components/App';
 import BrewContent from './components/BrewContent';
+import UserDetail from './components/UserDetail';
 import Thread from './components/Thread';
 
 const createRoutes = (props) => (
   <Route path='/' component={App} >
     <Route path='/b/:b' component={BrewContent} />
+    <Route path='/u/:u' component={UserDetail} />
     <Route path='/thread/:postId' component={Thread} />
   </Route>
 );
