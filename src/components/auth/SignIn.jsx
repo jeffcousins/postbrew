3b0531c29f35cb321d0cc76f3bdebc74dfaa58be
Jeff Cousins <jeffcousins@me.com>
2016-05-04 16:47:27 -0400

    Display authentication error message on client SignIn component

--- src/components/auth/SignIn.jsx
+++ src/components/auth/SignIn.jsx
@@ -1,39 +1,78 @@
 import React from 'react';
 import { reduxForm } from 'redux-form';
 import * as actions from '../../actions';
+import { Link } from 'react-router';
+import $ from 'jquery';
 
-const { object, func } = React.PropTypes;
+const { string, object, func } = React.PropTypes;
 
 const SignIn = React.createClass({
   propTypes: {
     fields: object,
     handleSubmit: func,
-    userSignIn: func
+    userSignIn: func,
+    errorMessage: string
+  },
+  componentDidMount () {
+    $('.ui.form')
+      .form({
+        on: 'blur',
+        fields: {
+          username: 'empty',
+          password: 'empty'
+        }
+      });
   },
   handleFormSubmit ({ username, password }) {
-    console.log(username, password);
     this.props.userSignIn({ username, password });
+  },
+  renderMessage () {
+    if (!this.props.errorMessage) {
+      return;
+    } else {
+      return (
+        <div className='ui attached fluid negative message'>
+          {this.props.errorMessage}
+        </div>
+      );
+    }
   },
   render () {
     const { handleSubmit, fields: { username, password } } = this.props;
 
     return (
-      <form className='ui form' onSubmit={handleSubmit(this.handleFormSubmit)}>
-        <div className='required six wide field'>
-          <label>Username:</label>
-          <input type='text' {...username} />
+      <div className='ui container compact'>
+        <div className='ui attached message'>
+          <div className='header'>
+            Welcome back!
+          </div>
+          <p>Sign in with the form below.</p>
         </div>
-        <div className='required six wide field'>
-          <label>Password:</label>
-          <input type='text' {...password} />
+        <form className='ui form attached fluid segment' onSubmit={handleSubmit(this.handleFormSubmit)}>
+          <div className='required field six wide'>
+            <label>Username:</label>
+            <input type='text' name='username' {...username} />
+          </div>
+          <div className='required field six wide'>
+            <label>Password:</label>
+            <input type='password' name='password' {...password} />
+          </div>
+          <button className='ui button positive' action='submit'>Sign In</button>
+        </form>
+        {this.renderMessage()}
+        <div className='ui bottom attached warning message'>
+          Don't have an account? <Link to='signup'><strong>Sign up here</strong></Link> instead.
         </div>
-        <button className='ui button positive' action='submit'>Sign In</button>
-      </form>
+      </div>
     );
   }
 });
 
+function mapStateToProps (state) {
+  return { errorMessage: state.auth.errorMessage };
+}
+
 export default reduxForm({
   form: 'signin',
   fields: ['username', 'password']
-}, null, actions)(SignIn);
+}, mapStateToProps, actions)(SignIn);
