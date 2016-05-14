c49c8b320a5ed5eea54648af253860b16be3b2f3
Jeff Cousins <jeffcousins@me.com>
2016-05-14 18:48:54 -0400

    Authenticate user if previously signed in

--- src/components/Root.jsx
+++ src/components/Root.jsx
@@ -4,6 +4,7 @@ import { Router, browserHistory } from 'react-router';
 import createRoutes from '../routes';
 import configStore from '../store/configStore';
 import { syncHistoryWithStore } from 'react-router-redux';
+import { IS_SIGNED_IN } from '../constants';
 
 // prevent server from trying to load CSS
 if (process.env.BROWSER) {
@@ -12,6 +13,13 @@ if (process.env.BROWSER) {
 }
 
 const store = configStore();
+
+const token = localStorage.getItem('token');
+
+if (token) {
+  store.dispatch({ type: IS_SIGNED_IN });
+}
+
 const history = syncHistoryWithStore(browserHistory, store);
 
 const Root = React.createClass({
