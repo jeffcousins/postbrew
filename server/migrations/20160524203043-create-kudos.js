486dac9c5b2bbe99b4b3ec740a2701fde4681395
Jeff Cousins <jeffcousins@me.com>
2016-05-24 16:25:10 -0500

    Create kudos model

---
+++ server/migrations/20160524203043-create-kudos.js
@@ -0,0 +1,27 @@
+'use strict';
+module.exports = {
+  up: function(queryInterface, Sequelize) {
+    return queryInterface.createTable('Kudos', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      value: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: function(queryInterface, Sequelize) {
+    return queryInterface.dropTable('Kudos');
+  }
+};
---
+++ server/models/kudos.js
@@ -0,0 +1,13 @@
+'use strict';
+module.exports = function(sequelize, DataTypes) {
+  var Kudos = sequelize.define('Kudos', {
+    value: DataTypes.INTEGER
+  }, {
+    classMethods: {
+      associate: function(models) {
+        // associations can be defined here
+      }
+    }
+  });
+  return Kudos;
+};
