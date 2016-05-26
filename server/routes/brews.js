4ed6bf14f38d5efde963bd0e4b6cffd19c4a6901
Jeff Cousins <jeffcousins@me.com>
2016-05-26 02:59:54 -0500

    Hook up route POST endpoint to create new brews

--- server/routes/brews.js
+++ server/routes/brews.js
@@ -1,12 +1,50 @@
 import models from '../models';
+import passport from 'passport';
+
+const requireAuth = passport.authenticate('jwt', { session: false });
 
 const brews = (app) => {
   app.route('/api/brews/create')
-    .post((req, res) => {
-      models.Brew.create(req.body)
-        .then((brew) => {
-          res.status(300).json(brew);
+    .post(requireAuth, (req, res) => {
+      const { brewName, title, description, userId } = req.body;
+
+      if (!brewName || !title || !description || !userId) {
+        return res.status(422).json({
+          errorMessage: 'Missing required fields and must be signed in.'
         });
+      }
+
+      models.Brew.findOne({
+        where: {
+          brew_name: brewName
+        }
+      }).then((existingBrew) => {
+        if (existingBrew) {
+          return res.status(422).send({
+            errorMessage: brewName + ' is already in use. ' +
+              'Please create a unique brew name.'
+          });
+        }
+
+        models.User.findOne({
+          where: {
+            id: userId
+          }
+        }).then(createBrew);
+      });
+
+      function createBrew (user) {
+        models.Brew.create({
+          brew_name: brewName.toLowerCase(),
+          title: title,
+          description: description,
+          founder: user.username
+        }).then((brew) => {
+          res.json({
+            brew: brew
+          });
+        });
+      }
     });
 
   app.route('/api/b/:brewId')
