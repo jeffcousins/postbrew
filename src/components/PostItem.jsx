5cfcc70721ba0d9972bb0c4553fcf5a2137e6a96
Jeff Cousins <jeffcousins@me.com>
2016-05-30 15:23:56 -0500

    Display relative timestamp, route to post's URL if it exists

--- src/components/PostItem.jsx
+++ src/components/PostItem.jsx
@@ -1,15 +1,43 @@
 import React from 'react';
 import { Link } from 'react-router';
+import moment from 'moment';
 
-const PostItem = (props) => (
-  <div className='item'>
-    <div className='content'>
-      <div className='header'>{props.data.title}</div>
-      <div className='description'>{props.data.username}</div>
-      <p>{props.data.createdAt}</p>
+const PostItem = (props) => {
+  console.log('post item props')
+  console.log(props);
+  const { title, username, createdAt, url, brew_name, id } = props.data;
+
+  function urlHandler (providedUrl, title, brew_name, id) {
+    if (!providedUrl) {
+      return (
+        <Link to={`/b/${brew_name}/${id}`}>{title}</Link>
+      );
+    } else {
+      if (providedUrl.slice(0, 7) !== 'http://') {
+        providedUrl = `http://${providedUrl}`;
+      }
+
+      return (
+        <a href={providedUrl}>{title}</a>
+      );
+    }
+  }
+
+  const timeAgo = moment(createdAt).startOf('hour').fromNow();
+
+  return (
+    <div className='item'>
+      <a href='www.reddit.com'>reddit</a>
+      <div className='content'>
+        <div className='header'>
+          {urlHandler(url, title, brew_name, id)}
+        </div>
+        <div className='description'>
+          Submitted {timeAgo} by {username}</div>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 const { object } = React.PropTypes;
 
