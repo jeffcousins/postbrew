79c1f34910e7f38b3b8e48aec87b6b37f20aea03
Jeff Cousins <jeffcousins@me.com>
2016-05-30 14:15:26 -0500

    Cleanup

--- src/reducers/brewContent.js
+++ src/reducers/brewContent.js
@@ -1,9 +1,6 @@
 const INITIAL_STATE = {};
 
 export default function (state = INITIAL_STATE, action) {
-  console.log('action received', action);
-  console.log('action.type ===', action.type);
-
   if (action.type === 'RECEIVED') {
     const newState = {};
     Object.assign(newState,
