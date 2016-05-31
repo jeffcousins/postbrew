5b49b05e4d4b97aa4aa97146d1958f19befa64e6
Jeff Cousins <jeffcousins@me.com>
2016-05-31 12:32:11 -0500

    Fetch all data if client is on root index.html

--- src/components/BrewContent.jsx
+++ src/components/BrewContent.jsx
@@ -3,31 +3,50 @@ import BrewHeader from './BrewHeader';
 import PostList from './PostList';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import { fetchBrewContent } from '../actions/index';
+import { fetchBrewContent, fetchAllContent } from '../actions/index';
+
 const { object, func, string } = React.PropTypes;
 
 const BrewContent = React.createClass({
   propTypes: {
     params: object,
     fetchBrewContent: func,
+    fetchAllContent: func,
     brewContent: object,
     brewpath: string,
     children: React.PropTypes.element
   },
   componentDidMount () {
-    this.props.fetchBrewContent(this.props.params.b);
+    if (!this.props.brewContent.posts) {
+      if (this.props.brewContent.pathname === '/') {
+        this.props.fetchAllContent();
+      } else {
+        this.props.fetchBrewContent(this.props.brewContent.pathname);
+      }
+    }
   },
   componentWillReceiveProps (nextProps) {
-    if (nextProps.brewpath !== this.props.brewpath) {
-      this.props.fetchBrewContent(nextProps.brewpath);
+    if (nextProps.brewContent.pathname !== this.props.brewContent.pathname) {
+      if (nextProps.brewContent.pathname === '/') {
+        this.props.fetchAllContent();
+      } else {
+        this.props.fetchBrewContent(nextProps.brewContent.pathname);
+      }
     }
   },
   render () {
-    if (!this.props.brewContent.title) {
+    if (this.props.brewContent.pathname === '/') {
+      return (
+        <div className='ui container'>
+          <BrewHeader />
+          <PostList />
+        </div>
+      )
+    } else if (!this.props.brewContent.title) {
       return (
         <div>
           <h3>No results found for {this.props.params.b}</h3>
-        </div>
+        </div>  
       );
     } else {
       return (
@@ -48,7 +67,7 @@ function mapStateToProps (state) {
 }
 
 function mapDispatchToProps (dispatch) {
-  return bindActionCreators({ fetchBrewContent }, dispatch);
+  return bindActionCreators({ fetchBrewContent, fetchAllContent }, dispatch);
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(BrewContent);
