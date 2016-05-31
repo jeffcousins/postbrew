b6fc750e444f4b303e9a12243c27a219698c374a
Jeff Cousins <jeffcousins@me.com>
2016-05-31 12:35:11 -0500

    Clear crucial brewContent props if loaded on root route

--- src/reducers/brewContent.js
+++ src/reducers/brewContent.js
@@ -12,8 +12,32 @@ export default function (state = INITIAL_STATE, action) {
       );
       return newState;
 
+    case 'RECEIVED_ALL':
+      Object.assign(newState,
+        state,
+        {
+          posts: action.payload.posts
+        }
+      );
+      return newState;
+
     case '@@router/LOCATION_CHANGE':
-      Object.assign(newState, state, action.payload);
+      let rootState = {};
+      if (action.payload.pathname === '/') {
+        rootState = {
+          UserId: '',
+          brew_name: '',
+          description: 'All posts.',
+          id: '',
+          pathname: '/',
+          title: '[ postbrew ]',
+          username: 'everyone',
+          createdAt: '',
+          updatedAt: ''
+        }
+      }
+
+      Object.assign(newState, state, rootState, action.payload);
       return newState;
   }
 
