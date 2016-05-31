5a24011c3b0715490e15342e8c5361b0bccdff56
Jeff Cousins <jeffcousins@me.com>
2016-05-31 17:54:37 -0500

    Moar constants

--- src/constants.js
+++ src/constants.js
@@ -3,3 +3,6 @@ export const IS_SIGNED_IN = 'IS_SIGNED_IN';
 export const IS_SIGNED_OUT = 'IS_SIGNED_OUT';
 export const SIGN_IN_ERROR = 'SIGN_IN_ERROR';
 export const LOCATION_CHANGE = '@@router/LOCATION_CHANGE';
+export const BREW_NOT_FOUND = 'BREW_NOT_FOUND';
+export const RECEIVED = 'RECEIVED';
+export const RECEIVED_ALL = 'RECEIVED_ALL';
