0dcc20d849ef785d159fc27189d561d84d0dec5a
Jeff Cousins <jeffcousins@me.com>
2016-06-01 02:44:25 -0500

    Create comment in db

--- server/routes/comments.js
+++ server/routes/comments.js
@@ -6,7 +6,31 @@ const requireAuth = passport.authenticate('jwt', { session: false });
 const comments = (app) => {
   app.route('/api/comments/submit')
     .post(requireAuth, (req, res) => {
-      return res.json(req.body);
+      const { userId, brewId, postId, content } = req.body;
+      let { parentId } = req.body;
+
+      if (!userId || !brewId || !postId || !parentId || !content) {
+        return res.status(422).json({
+          errorMessage: 'Missing required fields or must be signed in.'
+        });
+      }
+
+      if (parentId === -1) {
+        parentId = null;
+      }
+
+      models.Comment.create({
+        UserId: userId,
+        BrewId: brewId,
+        PostId: postId,
+        CommentId: parentId,
+        content: content,
+        kudos: 0
+      }).then((comment) => {
+        return res.json({
+          comment
+        });
+      });
     });
 }
 
