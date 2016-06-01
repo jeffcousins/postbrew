7f490bf4ee6069a0c4c5d52286f04e7039097fab
Jeff Cousins <jeffcousins@me.com>
2016-06-01 18:11:32 -0500

    Refactor.

--- src/components/CommentList.jsx
+++ src/components/CommentList.jsx
@@ -1,40 +1,22 @@
 import React from 'react';
-import moment from 'moment';
 import CommentItem from './CommentItem';
 
-const blue = {
-  color: '#2185D0'
-};
+const { array } = React.PropTypes;
 
 const CommentList = React.createClass({
+  propTypes: {
+    comments: array
+  },
   renderComments () {
-    if (this.props.comments && this.props.comments.length) {
-      return this.props.comments.map((comment) => {
-        const { id, username, content, createdAt } = comment;
-        const timeAgo = moment(createdAt).startOf('hour').fromNow();
-        return (
-          <div key={id} className='comment'>
-            <div className='content'>
-              <a className='author' style={blue}>{username}</a>
-              <div className='metadata'>
-                <span className='date'>{timeAgo}  </span>
-              </div>
-              <div className='text'>
-                {content}
-              </div>
-              <div className='actions'>
-                <a className='reply'>Reply</a>
-              </div>
-            </div>
-          </div>
-        );
-      });
-    }
+    return this.props.comments.map((comment) => {
+      return (
+        <CommentItem key={comment.id} {...comment} />
+      );
+    });
   },
   render () {
     return (
       <div className='ui threaded comments'>
-        <h3 className='ui dividing header'>Comments</h3>
         {this.renderComments()}
       </div>
     );
